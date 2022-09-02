56c06b20cea8c2f527f3ac3ae60af7fbc3a96692
Dmitrij <sdma7@mail.ru>
2022-09-02 14:47:46 +0300

    Задача номер 4

---
+++ Work004/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.WriteLine("Введите свое число ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while(num <= 2)
+{
+    Console.WriteLine("Введите другое число ");
+    num = Convert.ToInt32(Console.ReadLine());
+}
+while (count < num)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write($" {count}");
+        count = (count + 2);
+    }
+    else if (count % 2 == 1)
+    {
+        count = count + 1;
+    }
+}
+    
