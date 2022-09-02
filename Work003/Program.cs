2ed7727ef5d514b4cc3a41b23581cd3f10c4db22
Dmitrij <sdma7@mail.ru>
2022-09-02 13:11:35 +0300

    задача3 исправил решение

--- Work003/Program.cs
+++ Work003/Program.cs
@@ -1,11 +1,11 @@
 ﻿Console.WriteLine("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
-if (num % 2 == 1)
+if (num % 2 == 0)
 {
-    Console.WriteLine("Число " + num + " является: нечетным");
+    Console.WriteLine("Число " + num + " является: четным");
 }
 else
 {
-    Console.WriteLine("Число " + num + " является: четным");
+    Console.WriteLine("Число " + num + " является: нечетным");
 }
