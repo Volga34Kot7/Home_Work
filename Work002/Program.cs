39c1c194d23786ad9d68fb79e5debf0f74e6bba4
Dmitrij <sdma7@mail.ru>
2022-09-02 12:33:40 +0300

    задача2

---
+++ Work002/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int maximum = a;
+
+if (maximum < b)
+{
+        maximum = b;
+}
+else
+{
+        maximum = a;
+}
+if (maximum < c)
+{
+        maximum = c;
+}
+
+
+Console.Write(maximum);
